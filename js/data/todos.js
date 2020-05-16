536684eece30c621101f89a19e782ccf3f7a01e3
Edgariukstis <edgaras.banaitis@gmail.com>
2020-05-16 11:32:57 +0300

    render-list final

--- js/data/todos.js
+++ js/data/todos.js
@@ -8,13 +8,13 @@ let todo_list = [
     {
         description: 'Pravesti paskaita',
         created_on: '2020-05-05 15:02',
-        deadline:'2020-05-06 12:55',
-        status:'todo'
+        deadline:'2020-05-06 13:00',
+        status:'in-progress'
     },
     {
         description: 'Pradeti generuoti paskaitos irasus',
         created_on: '2020-05-05 15:03',
-        deadline:'2020-05-06 12:55',
-        status:'todo'
+        deadline:'2020-05-06 17:00',
+        status:'done'
     }
 ];
--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,22 @@
 "use strict";
 
+// render TODO list items
+function renderList( list ){
+    const listPlace = document.querySelector('.container');
+    
+    let HTML = '';
 
-console.log(todo_list);
+    for (let i = 0; i < list.length; i++) {
+        const todoItem = list[i];
+        
+        HTML += `
+                <div class="item">
+                    <div class="status ${todoItem.status}"></div>
+                    <p class="desciption">${todoItem.description}</p>
+                    <div class="deadline">${todoItem.deadline}</div>
+                </div>`;
+    }
+    return listPlace.innerHTML += HTML;;
+}
+
+renderList(todo_list);
